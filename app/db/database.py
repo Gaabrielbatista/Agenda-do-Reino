6d7ab400638c070247f7a185d9a170d43a285b26
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-05-07 19:20:22 -0300

    feat:db.create_all() removido para alembic

--- app/db/database.py
+++ app/db/database.py
@@ -1,4 +1,3 @@
-import time
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import OperationalError
 from app.config.settings import Config
@@ -8,15 +7,5 @@ db = SQLAlchemy()
 def init_db(app):
     app.config.from_object(Config)
     db.init_app(app)
-
-    with app.app_context():
-        for tentativa in range(10):
-            try:
-                db.create_all()
-                print("Banco conectado e tabelas")
-                break
-            except OperationalError:
-                print(f"Banco indisponível, tentativa {tentativa + 1}/10...")
-                time.sleep(2)
-        else:
-            print("Não foi possível conectar ao banco após 10 tentativas")
+    
+    print("Banco configurado com sucesso!")
