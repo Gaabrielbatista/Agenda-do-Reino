7ba84b3b9f80bc797c458675e2b2bedca21a6cdd
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-05-06 23:49:11 -0300

    feat: __init__.py para importar tabelas

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,3 @@
-from .usuario import Usuario, EventoNormal, EventoRecorrente
+from .usuario import Usuario
+from .evento_normal import EventoNormal, EventoStatus
+from .evento_recorrente import EventoRecorrente
