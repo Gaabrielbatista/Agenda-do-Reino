8f2e0530d27d46f3491f106cb3a56cef0222774b
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-05-07 18:46:31 -0300

    feat: criação do banco e tabelas

--- app/db/database.py
+++ app/db/database.py
@@ -1,4 +1,6 @@
+import time
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import OperationalError
 from app.config.settings import Config
 
 db = SQLAlchemy()
@@ -6,6 +8,15 @@ db = SQLAlchemy()
 def init_db(app):
     app.config.from_object(Config)
     db.init_app(app)
-    
+
     with app.app_context():
-        db.create_all()
+        for tentativa in range(10):
+            try:
+                db.create_all()
+                print("Banco conectado e tabelas")
+                break
+            except OperationalError:
+                print(f"Banco indisponível, tentativa {tentativa + 1}/10...")
+                time.sleep(2)
+        else:
+            print("Não foi possível conectar ao banco após 10 tentativas")
