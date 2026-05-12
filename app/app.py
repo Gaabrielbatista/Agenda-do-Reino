9074ebfaae9ce039ae05e1bc48d3049c9269b5f3
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-05-11 22:07:34 -0300

    feat bluenprint agenda_bp

--- app/app.py
+++ app/app.py
@@ -8,11 +8,14 @@ def create_app():
     # Blueprints
     from app.controllers.evento_normal_controller import evento_normal_bp
     from app.controllers.evento_recorrente_controller import evento_recorrente_bp
-    from app.controllers.usuario_controller import usuario_bp 
+    from app.controllers.usuario_controller import usuario_bp
+    from app.controllers.agenda_controller import agenda_bp
+
 
     app.register_blueprint(evento_normal_bp)
     app.register_blueprint(evento_recorrente_bp)
     app.register_blueprint(usuario_bp)
+    app.register_blueprint(agenda_bp)
 
     app.json.sort_keys = False
 
