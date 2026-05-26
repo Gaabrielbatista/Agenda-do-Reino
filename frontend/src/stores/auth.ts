b94a63d5976aba098ef9472d2dd8cefd950fe353
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-05-26 20:18:22 -0300

    limpa o user do localStorage

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -36,6 +36,7 @@ export const useAuthStore = defineStore('auth', {
       this.user = null
       this.token = null
       localStorage.removeItem('token')
+      localStorage.removeItem('user')
     }
   }
 })
