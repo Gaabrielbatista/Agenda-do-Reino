00d8f2b1562dae8c931083178c919cc863b6a5ef
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-05-28 20:25:46 -0300

    fix: CORS hardcoded removido para não quebrar em prod

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from app.db.database import init_db
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
+    CORS(app, supports_credentials=True, origins=os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(","))
     init_db(app, test_config)
     
     # Blueprints
