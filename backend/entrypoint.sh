4e5cdb2d5ff5a48af17d9cf7ae251a2d3d81d08b
Gaabrielbatista <ggabrielbattista@gmail.com>
2026-06-07 13:40:22 -0300

    fix: modificar caminho para executar o seed.py

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,7 +4,7 @@ echo "Rodando migrações do Alembic..."
 alembic upgrade head
 
 echo "Rodando o seed do banco de dados..."
-python -m app.seeds.seed_admin
+python seed.py
 
 echo "Iniciando Gunicorn..."
 exec gunicorn -w 4 -b 0.0.0.0:5000 "app.app:create_app()"
