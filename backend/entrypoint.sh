32af1310d079bcca0ee70472ddd4646be2aece73
Gabriel Batista <ggabrielbattista@gmail.com>
2026-06-08 22:30:35 -0300

    feat: adiciona suporte a IPv6 no Backend para o Railway

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,4 +7,4 @@ echo "Rodando o seed do banco de dados..."
 python seed.py
 
 echo "Iniciando Gunicorn..."
-exec gunicorn -w 4 -b 0.0.0.0:5000 "app.app:create_app()"
+exec gunicorn -w 4 -b 0.0.0.0:5000 -b [::]:5000 "app.app:create_app()"
